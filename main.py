489ca54a72f15646b1d35084b75c88d4280a5e00
Puckaboo <kayo.vanderheggen@gmail.com>
2021-06-05 14:32:12 +0200

    added try, except and finally

--- main.py
+++ main.py
@@ -3,56 +3,53 @@
 # imports
 #####################################################################################
 import psycopg2
+from psycopg2 import Error
 
 #####################################################################################
 # input
 #####################################################################################
-table_name = 'users'
+data_path = "./data/users.csv"
+table_name = "users"
 server_credentials= {
-    'dbname': "postgres", 
-    'user': "postgres", 
-    'password':"postgres", 
-    'host': "localhost", 
-    'port': "5432"
+    "dbname": "postgres", 
+    "user": "postgres", 
+    "password":"postgres", 
+    "host": "localhost", 
+    "port": "5432"
 }
 
-#####################################################################################
-# functions
-#####################################################################################
-def exists_table(table):
-    pass
 
-#####################################################################################
-# connect to postgresql server
-#####################################################################################
-conn = psycopg2.connect(**server_credentials)
+try:
+    # connect to the server
+    connection = psycopg2.connect(**server_credentials)
+    cursor = connection.cursor()
 
-#####################################################################################
-# create a data table
-#####################################################################################
-# issue commands using cursor
-cur = conn.cursor()
-
-# check if the table exists
-cur.execute("select exists(select * from information_schema.tables where table_name=%s)", (table_name,))
-table_exists = cur.fetchone()[0]
-
-# create the table and insert data
-if (not table_exists):
-    cur.execute(
-    """
-        CREATE TABLE users(
-        id integer PRIMARY KEY,
-        email text,
-        name text,
-        address text
+    # create the table
+    cursor.execute(
+        """
+            CREATE TABLE users(
+            id integer PRIMARY KEY,
+            email text,
+            name text,
+            address text
+        )
+        """
     )
-    """
-    )
-    conn.commit()
+    connection.commit()
+    print(f"successfully created a table named {table_name}")
 
-# insert the data from the .csv
-with open('./data/users.csv', 'r') as f:
-    next(f) # Skip the header row.
-    cur.copy_from(f, 'users', sep=',')
-conn.commit()
+    # add the data
+    with open(data_path, 'r') as f:
+        next(f) # Skip the header row.
+        cursor.copy_from(f, 'users', sep=',')
+    connection.commit()
+    print("successfully added the data from the .csv")
+
+except (Exception, Error) as error:
+    print("The following error occured: ", error)
+
+finally:
+    if (connection):
+        cursor.close()
+        connection.close()
+        print("PostgreSQL connection is closed")
