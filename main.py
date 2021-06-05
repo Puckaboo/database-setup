969386e879cdd4aac50920f79e230fe05d11f3c7
Puckaboo <kayo.vanderheggen@gmail.com>
2021-06-05 13:10:34 +0200

    create a table and commit the change

--- main.py
+++ main.py
@@ -4,3 +4,17 @@ import psycopg2
 
 # connect to server
 conn = psycopg2.connect(dbname="postgres", user="postgres", password="postgres", host="localhost", port="5432")
+
+# issue commands using cursor
+cur = conn.cursor()
+cur.execute(
+"""
+    CREATE TABLE users(
+    id integer PRIMARY KEY,
+    email text,
+    name text,
+    address text
+)
+"""
+)
+conn.commit()
