809351d24915afecaa607b20f05ce370b64a386b
Puckaboo <kayo.vanderheggen@gmail.com>
2021-06-05 14:10:22 +0200

    Inserting data from .csv

--- main.py
+++ main.py
@@ -1,20 +1,58 @@
 
+#####################################################################################
 # imports
+#####################################################################################
 import psycopg2
 
-# connect to server
-conn = psycopg2.connect(dbname="postgres", user="postgres", password="postgres", host="localhost", port="5432")
+#####################################################################################
+# input
+#####################################################################################
+table_name = 'users'
+server_credentials= {
+    'dbname': "postgres", 
+    'user': "postgres", 
+    'password':"postgres", 
+    'host': "localhost", 
+    'port': "5432"
+}
 
+#####################################################################################
+# functions
+#####################################################################################
+def exists_table(table):
+    pass
+
+#####################################################################################
+# connect to postgresql server
+#####################################################################################
+conn = psycopg2.connect(**server_credentials)
+
+#####################################################################################
+# create a data table
+#####################################################################################
 # issue commands using cursor
 cur = conn.cursor()
-cur.execute(
-"""
-    CREATE TABLE users(
-    id integer PRIMARY KEY,
-    email text,
-    name text,
-    address text
-)
-"""
-)
+
+# check if the table exists
+cur.execute("select exists(select * from information_schema.tables where table_name=%s)", (table_name,))
+table_exists = cur.fetchone()[0]
+
+# create the table and insert data
+if (not table_exists):
+    cur.execute(
+    """
+        CREATE TABLE users(
+        id integer PRIMARY KEY,
+        email text,
+        name text,
+        address text
+    )
+    """
+    )
+    conn.commit()
+
+# insert the data from the .csv
+with open('./data/users.csv', 'r') as f:
+    next(f) # Skip the header row.
+    cur.copy_from(f, 'users', sep=',')
 conn.commit()
