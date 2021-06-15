057883d346d12ad5cbfe7634dbb52d167931b700
Puckaboo <kayo.vanderheggen@gmail.com>
2021-06-15 10:55:14 +0200

    Reading in different files in separate tables

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 #####################################################################################
 # imports
 #####################################################################################
+import os
+import glob
+import pathlib
 import pandas as pd
 import numpy as np
 from sqlalchemy import create_engine
@@ -9,9 +12,8 @@ from sqlalchemy import create_engine
 #####################################################################################
 # input
 #####################################################################################
-data_path= "./data/data_small.csv"
-time_column= "S7 Leg SBFore Data Data Leg Time"
-table_name = "challenger"
+path= "/data-confidential/"
+separator= ";"
 dbname= "postgres"
 user= "postgres"
 password= "postgres" 
@@ -22,23 +24,31 @@ port= "5432"
 #####################################################################################
 # main program
 #####################################################################################
-df= pd.read_csv(data_path)
+cwd = os.getcwd()
+all_files = glob.glob(os.path.join(cwd+path, "*.csv"))
 
-df.insert(0, "epoch", pd.to_datetime(df[time_column],utc= True).values.astype(np.int64) // 10**6)
-print("successfully added time column to database")
+for f in all_files:
+    print(f"Adding the following file: {f}")
+    df= pd.read_csv(f, sep= separator)
+    
+    time_column = [s for s in df.columns if "Leg Time" in s][0]
+    df.insert(0, "epoch", pd.to_datetime(df[time_column],utc= True).values.astype(np.int64) // 10**6)
+    print("successfully added time column to database")
 
-postgres_url = f"postgresql://{user}:{password}@{host}:{port}/{dbname}"
-print("connecting to "+postgres_url)
+    postgres_url = f"postgresql://{user}:{password}@{host}:{port}/{dbname}"
+    print("connecting to "+postgres_url)
 
-try:
-    print("Uploading the data to postgres. This may take a while ...")
-    engine = create_engine(postgres_url)
-    df.to_sql(table_name, engine, if_exists='replace', index= False)
-except ValueError as vx:
-    print(vx)
-except Exception as ex:  
-    print(ex)
-else:
-    print("Postgresql database filled successfully.")
+    try:
+        print("Uploading the data to postgres. This may take a while ...")
+
+        table_name = f.split("/")[-1][0:-14]
+        engine = create_engine(postgres_url)
+        df.to_sql(table_name, engine, if_exists='replace', index= False)
+    except ValueError as vx:
+        print(vx)
+    except Exception as ex:  
+        print(ex)
+    else:
+        print("Postgresql database filled successfully.")
 
 
